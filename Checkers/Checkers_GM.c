7aec99e1cbd01840498b2135c9e9b6c68231a9c4
SkivHisink <49669372+SkivHisink@users.noreply.github.com>
2020-01-09 07:26:28 +0700

    Update Checkers_GM.c

--- Checkers/Checkers_GM.c
+++ Checkers/Checkers_GM.c
@@ -221,6 +221,7 @@ char** player_vs_player(char* field[], playerinf* Player1, playerinf* Player2, i
 		free(turn[i]);
 	}
 	free(turn);
+	return NULL;
 }
 
 char** player_vs_ai(char* field[], playerinf* Player1, playerinf* Player2, int p1ChClr, int p2ChClr, int numb_of_turn, int* ai_turns, int* ai_turns_2, int secondTurnChecker)
@@ -312,6 +313,7 @@ char** player_vs_ai(char* field[], playerinf* Player1, playerinf* Player2, int p
 		free(turn[i]);
 	}
 	free(turn);
+	return NULL;
 }
 
 char** ai_vs_ai(char* field[], playerinf* Player1, playerinf* Player2, int p1ChClr, int p2ChClr, int numb_of_turn, int* ai_turns, int* ai_turns_2, int secondTurnChecker)
